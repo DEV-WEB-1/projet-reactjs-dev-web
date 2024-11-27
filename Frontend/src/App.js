faa9d91bab9e6b8011b733fe83b1ff05a76a7193
OussamaMrabtini <oussamamrabtini@gmail.com>
2024-11-27 23:37:43 +0100

    app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/home" element={<Home />} />
